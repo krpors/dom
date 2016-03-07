f572b51a1c6502159b1565c1f2b665ba9aba61cf
Kevin Pors <kevin.pors@ing.nl>
2016-03-07 18:05:29 +0100

    Extra tests and some notes.

--- namednodemap.go
+++ namednodemap.go
@@ -1,5 +1,7 @@
 package dom
 
+import "fmt"
+
 type domNamedNodeMap struct {
 	nodes map[string]Node
 }
@@ -19,17 +21,29 @@ func (nnm *domNamedNodeMap) GetNamedItem(name string) Node {
 	return nnm.nodes[name]
 }
 
-// TODO: Return proper errors.
+// TODO: Return proper errors. The current implementation is not entirely correct.
+// The spec mentions that a NamedNodeMap can accept Nodes other than Attr, except
+// when the 'parent' is an Element. We'll need specializations of the NamedNodeMap
+// interface, for ex. attrNamedNodeMap, entityNamedNodeMap.
+//
+// See: https://www.w3.org/TR/2004/REC-DOM-Level-3-Core-20040407/core.html#ID-1780488922
+//
+// Errors:
+//
 // NUSE_ATTRIBUTE_ERR: Raised if arg is an Attr that is already an attribute of
 // another Element object. The DOM user must explicitly clone Attr nodes to
 // re-use them in other elements.
+//
 // HIERARCHY_REQUEST_ERR: Raised if an attempt is made to add a node doesn't
 // belong in this NamedNodeMap. Examples would include trying to insert something
 // other than an Attr node into an Element's map of attributes, or a non-Entity
 // node into the DocumentType's map of Entities.
 func (nnm *domNamedNodeMap) SetNamedItem(n Node) error {
-	nnm.nodes[n.GetNodeName()] = n
-	return nil
+	if _, ok := n.(Attr); ok {
+		nnm.nodes[n.GetNodeName()] = n
+		return nil
+	}
+	return fmt.Errorf("%v: can not set a non-Attr node as a named item", ErrorHierarchyRequest)
 }
 
 func (nnm *domNamedNodeMap) Length() int {
--- namednodemap_test.go
+++ namednodemap_test.go
@@ -3,5 +3,68 @@ package dom
 import "testing"
 
 func TestNamedNodeMap(t *testing.T) {
+	nnm := newNamedNodeMap()
+	if nnm.Length() != 0 {
+		t.Error("expected length of 0")
+	}
 
+	comment := newComment()
+	err := nnm.SetNamedItem(comment)
+	if err == nil {
+		t.Error("expected error at this point, but got none")
+	}
+
+	attr := newAttr()
+	attr.setName("name")
+	attr.SetValue("value")
+	nnm.SetNamedItem(attr)
+
+	if nnm.Length() != 1 {
+		t.Errorf("expected length of 1, got %d", nnm.Length())
+		t.FailNow()
+	}
+
+	ret := nnm.GetNamedItem("name")
+	if v, ok := ret.(Attr); ok {
+		if v.GetNodeValue() != "value" {
+			t.Errorf("expected 'value', got '%v'", v.GetNodeValue())
+		}
+	} else {
+		t.Error("type assertion for Attr failed")
+		t.FailNow()
+	}
+
+	attrDuplicate := newAttr()
+	attrDuplicate.setName("name")
+	attrDuplicate.SetValue("dupe!")
+	nnm.SetNamedItem(attrDuplicate)
+
+	if nnm.Length() != 1 {
+		t.Errorf("expected length of 1, got %d", nnm.Length())
+		t.FailNow()
+	}
+
+	ret = nnm.GetNamedItem("name")
+	if v, ok := ret.(Attr); ok {
+		if v.GetNodeValue() != "dupe!" {
+			t.Errorf("expected 'dupe!', got '%v'", v.GetNodeValue())
+		}
+	} else {
+		t.Error("type assertion for Attr failed")
+		t.FailNow()
+	}
+
+	m := nnm.GetItems()
+	if len(m) != 1 {
+		t.Errorf("expected length of 1, got %d", len(m))
+		t.FailNow()
+	}
+
+	if v, found := m["name"]; found {
+		if v.GetNodeValue() != "dupe!" {
+			t.Errorf("expected 'dupe!' got '%v'", v.GetNodeValue())
+		}
+	} else {
+		t.Error("expected to find key 'name', but got nothing")
+	}
 }
